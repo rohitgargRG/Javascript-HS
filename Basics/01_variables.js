65dc4f85d2837db57a1c54f98e1b4bb5bddf94dd
Rohit Garg <124335028+rohitgargRG@users.noreply.github.com>
2023-09-22 19:11:00 +0530

    variables

---
+++ Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "rg@gmail.com"
+var accountPassword = "1234"
+accountCity = "Nagpur"
+let accountState;
+
+// accountId = 5555
+
+accountEmail = "sds@a.com"
+accountPassword = 23232
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var
+beacause of block scope and variable scope
+*/
