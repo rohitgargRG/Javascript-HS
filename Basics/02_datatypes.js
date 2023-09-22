056ee609253c8047e67b731d1e0a6a1d8133f939
Rohit Garg <124335028+rohitgargRG@users.noreply.github.com>
2023-09-22 19:09:58 +0530

    datatypes

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3+3) // we are using nodeJs not browser
+
+// refer mdn and tc39
+console.log(3+3)
+
+console.log("hi")
+
+let name = "rohit" // string datatype
+let age = 20  // number datatype
+let isLoggedIn = false //bool
+let state = null;
+
+// Primitive DataTypes -->
+
+// number  -- range = 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (means empty)
+// undefined => means that i have created a variable 
+//              but haven't assigned a value to it yet
+// symbol => unique
+
+// Non-Primitive DataTypes --
+
+// object
+
+console.log(typeof "rg");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
